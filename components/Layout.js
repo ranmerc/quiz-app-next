2ee3467d11cc80c52fd1d4d8523fa654d7683781
Kamran Ansari <crossthinking786@gmail.com>
2021-05-19 21:58:26 +0530

    added meta description

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,7 @@ export default function Layout({ children, title }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Quiz app built with next.js" />
         <title>{title}</title>
       </Head>
       {children}
